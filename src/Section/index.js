b29e14373e24e72ee6f44ac059fa737c4147e9de
Tomasz Franusik <franusiktomasz>
2023-03-14 12:50:49 +0100

    renaming a class in the Section folder

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,9 +2,9 @@ import "./style.css"
 
 const Section = (props) => (
     <section className="section">
-        <div className="section__div">
-        <h2 className="section__header">{props.title}</h2>
-        {props.extraHeaderContent}
+        <div className="section__header">
+            <h2 className="section__title">{props.title}</h2>
+            {props.extraHeaderContent}
         </div>
         {props.body}
     </section>
